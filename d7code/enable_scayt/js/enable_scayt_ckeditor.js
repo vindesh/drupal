e6c3c9a4669f4ff2e621a3929936afe6927451b6
Vindesh Mohariya <vindeshm@gmail.com>
2021-10-07 15:15:49 +0530

    drupal 7 module code

---
+++ d7code/enable_scayt/js/enable_scayt_ckeditor.js
@@ -0,0 +1,6 @@
+CKEDITOR.editorConfig = function( config ) {
+  // Allow the browser to do the spellchecking.
+  config.disableNativeSpellChecker = false;
+  // Start SCAYT
+  config.scayt_autoStartup = true;
+};
